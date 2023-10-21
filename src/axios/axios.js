85487af490154040258cc39f9dda603a0d2ad988
arun <arunrajrmvnr@gmail.com>
2023-10-21 11:28:30 +0530

    def

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,24 +1,24 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:8000/backend/",
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //     Authorization: localStorage.getItem("authToken")
-  //       ? "Bearer " + JSON.parse(localStorage.getItem("authToken"))
-  //       : null,
-  //     accept: "application/json",
-  //   },
-});
 // const axiosInstance = axios.create({
-//   baseURL: "https://ant-space-fitness.azurewebsites.net/backend/",
-//   // headers: {
-//   //   "Content-Type": "application/json",
-//   //   Authorization: localStorage.getItem("authToken")
-//   //     ? "Bearer " + JSON.parse(localStorage.getItem("authToken"))
-//   //     : null,
-//   //   accept: "application/json",
-//   // },
+//   baseURL: "http://127.0.0.1:8000/backend/",
+//   //   headers: {
+//   //     "Content-Type": "application/json",
+//   //     Authorization: localStorage.getItem("authToken")
+//   //       ? "Bearer " + JSON.parse(localStorage.getItem("authToken"))
+//   //       : null,
+//   //     accept: "application/json",
+//   //   },
 // });
+const axiosInstance = axios.create({
+  baseURL: "https://ant-space-fitness.azurewebsites.net/backend/",
+  // headers: {
+  //   "Content-Type": "application/json",
+  //   Authorization: localStorage.getItem("authToken")
+  //     ? "Bearer " + JSON.parse(localStorage.getItem("authToken"))
+  //     : null,
+  //   accept: "application/json",
+  // },
+});
 
 export default axiosInstance;
